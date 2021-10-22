72bb7602c57f0b4bcdb8a14a6617b033428a16da
nikhiltiwari <nikhiltiwari507@gmail.com>
2021-10-22 18:30:58 +0530

    nikhil

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AddRoleComponent } from './components/add-role/add-role.component';
+import { AddUserByRoleComponent } from './components/add-user-by-role/add-user-by-role.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    AddRoleComponent
+    AddRoleComponent,
+    AddUserByRoleComponent
   ],
   imports: [
     CommonModule,
